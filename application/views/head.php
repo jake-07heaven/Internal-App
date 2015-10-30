be32527f60bbd28632195ae8823f7f1111480978
Jake Alder <marketing@192.168.9.41>
2015-10-30 12:38:29 +0000

    Time Changed

--- application/views/head.php
+++ application/views/head.php
@@ -5,17 +5,18 @@
         <!--<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7578754/725886/css/fonts.css">-->
 	<link href='https://fonts.googleapis.com/css?family=Cabin:400,500,600,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/styles.css">
+        <script src="<?php echo base_url(); ?>js/moment.js"></script>
 	<script type="text/javascript"> 
 	function display_c(){
 	var refresh=1000; // Refresh rate in milli seconds
-	mytime=setTimeout('display_ct()',refresh)
+	mytime=setTimeout('display_ct()',refresh);
 	}
-
-	function display_ct() {
-	var strcount
-	var x = new Date()
-	document.getElementById('ct').innerHTML = x;
-	tt=display_c();
-	}
+        function display_ct() {
+            var y = moment().format('HH:mm:ss');
+            var x = moment().format('dddd, DD[/]MM[/]YY');
+            document.getElementById('ct').innerHTML = x;
+            document.getElementById('t').innerHTML = y;
+            tt=display_c();
+        }
 	</script>
 </head>
--- application/views/header.php
+++ application/views/header.php
@@ -3,6 +3,7 @@
 		<div class="container">
 			<a href="<?php echo base_url(); ?>index.php/dashboard"><img class="logo" src="http://cdn2.hubspot.net/hub/385095/file-1148947077-png/img/logo.png" alt="07 Heaven Digital Marketing"></a>
 			<div class="right header-right">
+				<span id='t'></span>
 				<span id='ct'></span>
 				<a class="header-button" href="<?php echo base_url(); ?>index.php/dashboard/logout">Logout</a>
 			</div>
